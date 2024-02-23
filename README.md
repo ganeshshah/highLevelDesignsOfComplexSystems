c98e7a9bd16b3f195766dc25d5d964803134f36c
ganeshshah <shahganesh777@gmail.com>
2024-02-23 12:05:58 +0530

    dded pen design structure

--- README.md
+++
@@ -1,2 +0,0 @@
-# HLD and LLD of complex software applications
-Repository to create low-level and high-level designs of complex software applications
---
+++ pen/src/main/java/com/systemdesign/pen/PenApplication.java
@@ -0,0 +1,13 @@
+package com.systemdesign.pen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PenApplication.class, args);
+	}
+
+}
